5f9b1f1a9480ae661f1465d9f24d7c9e2fa732be
root <marcosf40@hotmail.com>
2017-07-10 15:23:58 -0300

    Correcoes e comentarios

--- clientTP3.py
+++ clientTP3.py
@@ -1,46 +1,61 @@
-import socket   #for sockets
-import sys  #for exit
+import socket   
+import sys  
 import struct
 import binascii
- 
-# create dgram udp socket
-try:
-    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP)
-    s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-    s.bind(('', 2222))
-except socket.error:
-    print 'Failed to create socket'
-    sys.exit()
- 
-host = sys.argv[1]
-port = int(sys.argv[2])
- 
-while(1) :
-    msg_r = raw_input('Enter message to send : ')
-    header = struct.pack('!H', 1)
-    msg =  header + msg_r   
- 
-    
-    try :
-        #Set the whole string
-        s.sendto(msg, (host, port))
+class Client(object):
 
-        # receive data from client (data, addr)
-        m, d = s.recvfrom(1024)
-        
-        header = struct.pack('!2s', m)
-        print "mensagem",header
-        print "recv", d
-        #reply = d[0]
-        #addr = d[1]
-         
-        #print 'Server reply : ' + reply
-        #m = s.recvfrom(1024)
-        #print m
-     
-    except socket.error, msg:
-        print 'Error Code : ' + str(msg[0]) + ' Message ' + msg[1]
-        sys.exit()
+    def __init__(self, host, port):
+        self.s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP)
+        self.s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        self.s.bind(('', 2222))
+        self.host = host
+        self.port = port
+
+    def sen(self, msg):
+        try:
+            header = struct.pack('!H', 1)        
+            msg =  header + msg_r
+            self.s.sendto(msg, (self.host, self.port))
+            self.s.settimeout(4)
+        except socket.timeout:          
+            print("closing socket")
+            
+
+    def send(self, msg):
+        self.sen(msg)
+        Restramitir = True 
+        try:
+            while (Restramitir):
+                data, d = self.s.recvfrom(1024)
+                if data:
+                    msg_type = struct.unpack('!H',data[:2])[0]
+                    value = data[2:]
+                    print '\nResposta de %s na porta %d:'% d
+                    print value
+                else:
+                    print '\nErro na mensagem'
+                    print msg_type
+                    print value
+                    print "recv", msg_type,d
+            Restramitir = False                    
+                    
+        except socket.timeout:       
+            Restramitir = False
+            self.sen(msg)
+            print("Nada a Receber")
 
 
-s.close()
+
+
+
+if __name__ == '__main__':
+    if len(sys.argv) != 2:
+        sys.exit('Numero incorreto de argumentos.\nUso: <IP:port>')
+    else:
+        ip,port = sys.argv[1].split(':')
+        client = Client(ip, int(port))
+        msg_r = raw_input('Enter message to send : ')
+
+        client.send(msg_r)
+
+
--- servetnTP3.py
+++ servetnTP3.py
@@ -3,10 +3,11 @@ import sys
 import struct
 import binascii
 MSG_TYPE_SIZE = 2
+import re
 
 class Servent(object):
     """docstring for Servent"""
-    def __init__(self, recv_port, file ):   
+    def __init__(self, recv_port, file ):  
         self.host = socket.gethostbyname(socket.gethostname())
         self.port = recv_port
         self.server = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP) # Create Datagram Socket (UDP)
@@ -18,40 +19,55 @@ class Servent(object):
         print "dicionario msg", self.dicionario_mensagem
         self.ttl = 3
         self.count = 0
+        self.input_file = file
+        self.values = self.read_file(self.input_file)
 
-    def set_neighbors(self, neighbors): 
+    def set_neighbors(self, neighbors):
         self.neighbors_dict = neighbors
-        
+
+    def decoder_RESPONSE(self, value, ipcliente, portoClient):
+        ''' Send response to Client '''
+        msg_RESPONSE = struct.pack('!H', 3)
+        msg_send_ = str(msg_RESPONSE) + str(value)
+        self.server.sendto(msg_send_, (''+ipcliente+'' , int(portoClient)))
+    
+    def verifica_chave(self, key, ipcliente, portoClient):
+        ''' check value in dict'''
+        print 'Procurando por:', key
+        if key in self.values:
+            value = self.values[key]
+            print 'Valor de',key, 'encontrado:',value
+            self.decoder_RESPONSE(value,ipcliente, portoClient)            
+        else:
+            print 'Valor de',key, 'nao encontrado.'
 
     def get_neighbors(self):
         return self.neighbors_dict
 
-    def read_file(self):
-        print "files"
+    def read_file(self, file):
+        get = re.compile(r'^\s*([^#\s][^\s]*)\s*([^\s].*[^\s]|[^\s])\s*$')
+        try:
+            with open(file, 'r') as f:
+                return {line.group(1) : line.group(2)
+                        for line in map(get.match, f) if line is not None}
+        except IOError:
+            print >>sys.stderr, 'Error ao tentar ler arquivo.'
+        sys.exit(0)
         
-    def msg_query(self):
-        self.ttl = 3
-
 
     def decode_CLIRESQ(self, chave, address):
+        ''' Recev CLIRESQ'''
         nei = self.get_neighbors()
         msg_type_query = struct.pack('!H', 2)
         ipcliente = address[0]
         ip_inet = socket.inet_aton(ipcliente)  
-        portoClient = address[1]        
-        ip = address
-        port = 2222
-        
+        portoClient = address[1]
         chave = chave      
-        
-        #if (ipcliente, portoClient, self.seq_number, chave) not in self.dicionario_mensagem:
+        ''' Add data in dict of mensagem'''       
         self.dicionario_mensagem.add((ipcliente, portoClient, self.seq_number, chave))
-        #print "dicionario msg", self.dicionario_mensagem
-            # verifica no arquivo
-            #self.__check_for_chave((ip, port), chave)
-        self.seq_number += 1
-        print self.seq_number
-        send_query = str(msg_type_query) + str(self.ttl)  + str(self.seq_number) + str(port) + str(ip_inet) + str(chave) 
+        self.seq_number += 1       
+        send_query = str(msg_type_query) + str(self.ttl)  + str(self.seq_number) + str(portoClient) + str(ip_inet) + str(chave) 
+
         for x in nei:            
             split_x = str(x).split(":")
             dest_source = (''+split_x[0]+'' , int(split_x[1]))                     
@@ -61,95 +77,62 @@ class Servent(object):
     def decode_QUERY(self, msg):
         nei = self.get_neighbors()
         msg_type_query = struct.pack('!H', 2)
-
-        self.count +=1 
-        print "count: ", self.count
-        print "+++++++\n\n" , msg
-        
-
-        type_msg =  msg[:1] 
-        print "TYPE MSG: ", type_msg    
-        ttl = msg[1:2]
-        print "TTL", ttl
-        n_sequencia = msg[2:3]
-        print "N SEQUANDI", n_sequencia
-        
-        portoClient = msg[2:6]      
-        print "PORT CLIENTE", portoClient
-        ipcliente = socket.inet_ntoa(msg[6:10])
-        print "IP CLIENTE", ipcliente
+        ''' Processing of data'''
+        self.ttl =  msg[:1]       
+        self.seq_number = msg[1:2]       
+        type_msg = msg[2:3]       
+        portoClient = msg[2:6]  
+        ipcliente = socket.inet_ntoa(msg[6:10])       
         chave = msg[10:]
-        print "CHAVE", chave
+
         ip_inet = socket.inet_aton(ipcliente)
-     
-        #msg_t = msg[:1]
-        #print msg_t
-        #dd = socket.inet_ntoa(msg[2:6])
-        #print dd
-       # 2310,0,0,31
-        print "dicionario: ", self.dicionario_mensagem
-        print ipcliente, portoClient, n_sequencia, 
+        # reliable flooding
+        if (ipcliente, portoClient, self.seq_number, chave) not in self.dicionario_mensagem:
 
-        if (ipcliente, portoClient, n_sequencia, chave) not in self.dicionario_mensagem:
-            self.dicionario_mensagem.add((ipcliente, portoClient, n_sequencia, chave))
-            print "dicionario msg", self.dicionario_mensagem
-
-        elif (ipcliente, portoClient, n_sequencia, chave) in self.dicionario_mensagem:
-            print "Ja tem"
-
-        
-
-        int(ttl)  
-        print ttl
-
-        if self.ttl > 0 :
-            # verifica no arquivo
-            #self.__check_for_chave((ip, port), chave)
-        #self.seq_number += 1
+            self.dicionario_mensagem.add((ipcliente, portoClient, self.seq_number, chave))          
             send_query = str(msg_type_query) + str(self.ttl)  + str(self.seq_number) + str(portoClient) + str(ip_inet) + str(chave) 
-            for x in nei:            
-                split_x = str(x).split(":")
-                dest_source = (''+split_x[0]+'' , int(split_x[1]))                     
-                self.s.sendto(send_query, (dest_source))
+            ttl_local = int(self.ttl) 
 
+            ttl_local = ttl_local - 1
+            self.ttl = ttl_local
 
+            '''Verify ttl '''
+            if self.ttl > 0 :
+                ''' Search in local dict'''
+                self.verifica_chave(chave, ipcliente, portoClient)
+                ''' Send to neighbors '''
+                for x in nei:            
+                    split_x = str(x).split(":")
+                    dest_source = (''+split_x[0]+'' , int(split_x[1]))                    
+                    self.s.sendto(send_query, (dest_source))
+            
+        elif (ipcliente, portoClient, self.seq_number, chave) in self.dicionario_mensagem:
+            print "Pesquisa ja realizada."
 
-        m = "OK"
-        dest_source = (''+ipcliente+'' , int(portoClient)) 
-        self.server.sendto(m, (dest_source))
 
         
     def decode_msg(self, data, address_recv):
+        # Processing of data
         msg_type = struct.unpack('!H',data[:2])[0]
         chave = data[2:1024]
-        #print "\nreceived message:", chave
-        #print '\nfrom:',address_recv
-        #print '\nmsg type', msg_type
-
         if (msg_type == 1):            
             self.decode_CLIRESQ(chave,address_recv)
         elif msg_type == 2:
-            print "Mensagem 2"
             self.decode_QUERY(chave)
 
     def recvfrom(self):
-        #print "\nWaint...."
-        #print "\nConexao :", self.port 
-        #print "\nIP: ", self.host  
+        print "\nWaint...."
         while (True):
-            message, address = self.server.recvfrom(1024) # Buffer size is 8192. Change as needed. 
-                
-            if message:                
-                #print "addres", address
+            message, address = self.server.recvfrom(1024)                 
+            if message:  
                 self.decode_msg(message, address)              
             
 
 
-def main():
-    
+def main():    
     if len(sys.argv) < 3:
-        sys.exit('Numero insufi ciente de argumentos.\n' + 
-            'Uso: <localport> <chave-values> <ip1:port1> ... <ipN:portN>')
+        sys.exit('Numero insuficiente de argumentos.\n' + 
+            'Ex: <localport> <chave-values> <ip1:port1> ... <ipN:portN>')
     else:
         port = int(sys.argv[1])
         input_file = sys.argv[2]
